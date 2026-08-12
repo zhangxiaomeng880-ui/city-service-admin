a461d5dd89fb963f7dc5b895936e329c953d33e3
zhangxiaomeng880-ui <zhangxiaomeng880@gmail.com>
2026-08-12 20:11:46 +0800

    feat: apply Figma sketch visual baseline

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./app/App";
 import "./styles/index.css";
+import "./styles/figma-baseline.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
