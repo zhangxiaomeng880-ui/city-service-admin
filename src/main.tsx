8eb3b4d6432fc110145ff318b146566fd8883bd5
zhangxiaomeng880-ui <zhangxiaomeng880@gmail.com>
2026-08-13 16:50:20 +0800

    feat: load responsive optimization stylesheet

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./app/App";
 import "./styles/index.css";
 import "./styles/figma-baseline.css";
+import "./styles/responsive.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
